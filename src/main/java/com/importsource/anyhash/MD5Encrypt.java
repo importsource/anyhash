c4aa9980869af8ce2fccd0908f1916f6644aab13
importsource <hezhuofan24@126.com>
2016-08-17 09:15:56 +0800

    change demo

--- src/main/java/com/importsource/anyhash/MD5Encrypt.java
+++ src/main/java/com/importsource/anyhash/MD5Encrypt.java
@@ -1,95 +1,81 @@
 package com.importsource.anyhash;
+
 import java.security.MessageDigest;
- 
- /**
-  * MD5
-  * 
-  * @author Hezf
-  *
-  */
- public class MD5Encrypt {
- 
- 	public MD5Encrypt() {
- 	}
- 
- 	private final static String[] hexDigits = {
- 
- 			"0", "1", "2", "3", "4", "5", "6", "7",
- 
- 			"8", "9", "a", "b", "c", "d", "e", "f" };
- 
- 	public static String byteArrayToString(byte[] b) {
- 
- 		StringBuffer resultSb = new StringBuffer();
- 
- 		for (int i = 0; i < b.length; i++) {
- 			resultSb.append(byteToHexString(b[i]));
- 			// resultSb.append(byteToNumString(b[i]));
- 		}
- 
- 		return resultSb.toString();
- 
- 	}
- 
- 	@SuppressWarnings("unused")
- 	private static String byteToNumString(final byte b) {
- 		int _b = b;
- 
- 		if (_b < 0) {
- 
- 			_b = 256 + _b;
- 
- 		}
- 
- 		return String.valueOf(_b);
- 
- 	}
- 
- 	private static String byteToHexString(byte b) {
- 
- 		int n = b;
- 
- 		if (n < 0) {
- 
- 			n = 256 + n;
- 
- 		}
- 
- 		int d1 = n / 16;
- 
- 		int d2 = n % 16;
- 
- 		return hexDigits[d1] + hexDigits[d2];
- 
- 	}
- 
- 	public static String MD5Encode(String origin) {
- 
- 		String resultString = null;
- 
- 		try {
- 
- 			resultString = new String(origin);
- 
- 			MessageDigest md = MessageDigest.getInstance("MD5");
- 
- 			resultString = byteArrayToString(md.digest(resultString.getBytes()));
- 
- 		}
- 
- 		catch (Exception ex) {
- 
- 			ex.printStackTrace();
- 
- 		}
- 
- 		return resultString;
- 
- 	}
- 
- 	public static void main(String[] args) {
- 		// MD5Encrypt md5encrypt = new MD5Encrypt();
- 		System.out.println(MD5Encode("123456"));
- 	}
- 
- }
+
+/**
+ * MD5
+ * 
+ * @author Hezf
+ */
+public class MD5Encrypt {
+	public MD5Encrypt() {
+	}
+
+	private final static String[] hexDigits = {
+
+			"0", "1", "2", "3", "4", "5", "6", "7",
+
+			"8", "9", "a", "b", "c", "d", "e", "f" };
+
+	public static String byteArrayToString(byte[] b) {
+		StringBuffer resultSb = new StringBuffer();
+
+		for (int i = 0; i < b.length; i++) {
+			resultSb.append(byteToHexString(b[i]));
+			// resultSb.append(byteToNumString(b[i]));
+		}
+
+		return resultSb.toString();
+
+	}
+
+	@SuppressWarnings("unused")
+	private static String byteToNumString(final byte b) {
+		int _b = b;
+
+		if (_b < 0) {
+
+			_b = 256 + _b;
+
+		}
+
+		return String.valueOf(_b);
+
+	}
+
+	private static String byteToHexString(byte b) {
+
+		int n = b;
+
+		if (n < 0) {
+
+			n = 256 + n;
+
+		}
+
+		int d1 = n / 16;
+
+		int d2 = n % 16;
+
+		return hexDigits[d1] + hexDigits[d2];
+
+	}
+
+	public static String MD5Encode(String origin) {
+		String resultString = null;
+		try {
+			resultString = new String(origin);
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			resultString = byteArrayToString(md.digest(resultString.getBytes()));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return resultString;
+	}
+
+	public static void main(String[] args) {
+		// MD5Encrypt md5encrypt = new MD5Encrypt();
+		System.out.println(MD5Encode("123456"));
+	}
+
+}
--- src/main/java/com/importsource/anyhash/demo/Demo.java
+++ src/main/java/com/importsource/anyhash/demo/Demo.java
@@ -21,16 +21,23 @@ public class Demo {
 	 */
 	public static void main(String[] args) {
 		Collection<String> c=new ArrayList<String>();
-		c.add("1");
-		c.add("2");
-		c.add("3");
-		c.add("4");
-		c.add("5");
-		c.add("6");
-		Hashable hash=new ConsistentHash(new HashFunction(), 10, c);
-		String result=hash.get("name1").toString();
+		c.add("125.0.2.5");
+		c.add("125.0.2.6");
+		c.add("125.0.2.7");
+		c.add("125.0.2.8");
+		c.add("125.0.2.9");
+		c.add("125.0.2.10");
+		Hashable<String> hash=new ConsistentHash<String>(new HashFunction(), 10, c);
+		String result=hash.get("aorun").toString();
+		
    
 		System.out.println(result);
+		hash.add("125.0.2.11");
+		String result1=hash.get("aorun").toString();
+		
+		   
+		System.out.println(result1);
+		
 	}
 
 }
